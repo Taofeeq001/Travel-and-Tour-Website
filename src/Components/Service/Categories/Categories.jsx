63225791506afd5823cd08e8ce7d4b6e49c0d11e
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-08-03 10:02:06 -0400

    updating Categories

--- src/Components/Service/Categories/Categories.jsx
+++ src/Components/Service/Categories/Categories.jsx
@@ -6,7 +6,7 @@ import plane from '../../../Images/plane.png';
 import rel from '../../../Images/hands 1.png';
 import medical from '../../../Images/medical-team 1.png'
 import rectangle from '../../../Images/Rectangle 157.png'
-
+import honey from '../../../Images/Illustration.png'
 
 const Categories = () =>{
     return(
@@ -39,6 +39,15 @@ const Categories = () =>{
                     paragraph = 'sunt qui repellat saepe quo velit aperiam id aliquam placeat.'
                     class = "four"
                 />
+            </div>
+            <div className="romantic-tropical">
+                <img className="romantic-image" src={honey} alt="" />
+                <div className="romantic-content">
+                    <h4>HONEYMOON SPECIALS</h4>
+                    <h1>Our Romantic Tropical Destinations</h1>
+                    <p>Et labore harum non nobis ipsum eum molestias mollitia et corporis praesentium a laudantium internos. Non quis eius quo eligendi corrupti et fugiat nulla qui soluta recusandae in maxime quasi aut ducimus illum aut optio quibusdam!</p>
+                    <button className="desktop-btn">View Packages</button>
+                </div>
             </div>
         </div>
     )
